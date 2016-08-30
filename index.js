396aa99d89259922011024bef8ceaba293529158
Jim Bulkowski <jim.b@paperelectron.com>
2016-08-30 15:56:29 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-pg
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+/**
+ *
+ * @module index
+ */
+
+module.exports = [
+  require('pomegranate-sequelize-core'),
+  require('./plugins/SqliteStorage'),
+  require('./plugins/loadModels')
+];
---
+++ plugins/SqliteStorage.js
@@ -0,0 +1,43 @@
+/**
+ * Created by monstertke on 8/30/16.
+ */
+
+/**
+ * @file loadModels
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project Pomegranate-sequelize-pg
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+'use strict';
+var path = require('path');
+
+/**
+ *
+ * @module loadModels
+ */
+
+exports.options = {
+  filename: 'data.sqlite',
+  workDir: './sqlitestore'
+}
+
+exports.metadata = {
+  name: 'SQLiteStorage',
+  param: 'SQLStore',
+  type: 'service',
+  depends: ['SequelizeCore']
+}
+
+exports.plugin = {
+  load: function(inject, loaded) {
+    var storageFile = path.join(this.options.workDir, this.options.filename)
+    loaded(null, storageFile)
+  },
+  start: function(done) {
+    done()
+  },
+  stop: function(done) {
+    done()
+  }
+};
